14639270f7331c304f723e87a7190a25875db383
Ollie Chick <och26@uclive.ac.nz>
2019-05-08 23:21:06 +1200

    update splitting

--- split.sh
+++ split.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
-ffmpeg -i "${1:-video.mp4}" -map 0 -c copy -f segment -segment_time ${2:-1800} "${3:-output}_%03d.mp4"
+#ffmpeg -i "${1:-video.mp4}" -ss 60 -to 80 -c copy "${2:-output}_%03dsdfjkfjskljkldsf.mp4"
+
+start_time=0
+
+for var in "${@:3}"
+do
+    end_time="$var"
+    echo "$start_time - $end_time"
+    ffmpeg -i "${1:-video.mp4}" -ss ${start_time} -to ${end_time} -c copy "${2:-output}_${start_time}.mp4"
+    start_time=${end_time}
+done
