081020f09b4afae7ed89d67b2008e75957b59397
Ollie Chick <och26@uclive.ac.nz>
2019-05-11 15:19:23 +1200

    Use _i for split filenames to guarantee filenames are valid

--- split.sh
+++ split.sh
@@ -3,11 +3,13 @@
 #ffmpeg -i "${1:-video.mp4}" -ss 60 -to 80 -c copy "${2:-output}_%03dsdfjkfjskljkldsf.mp4"
 
 start_time=0
+i=0
 
 for var in "${@:3}"
 do
     end_time="$var"
     echo "$start_time - $end_time"
-    ffmpeg -i "${1:-video.mp4}" -ss ${start_time} -to ${end_time} -c copy "${2:-output}_${start_time}.mp4"
+    ffmpeg -i "${1:-video.mp4}" -ss ${start_time} -to ${end_time} -c copy "${2:-output}_${i}.mp4"
     start_time=${end_time}
+    i=$((i+1))
 done
